bdc851128772a309326533ab9e52c772012266bb
U-Hoppinger <hopp1nger@yandex.ru>
2026-08-15 15:25:37 +0300

    first

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,18 +1,52 @@
-import {getPages} from "../lib/utils.js";
+import { getPages } from "../lib/utils.js";
 
-export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
-    // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+export const initPagination = (
+  { pages, fromRow, toRow, totalRows },
+  createPage,
+) => {
+  // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+  const pageTemplate = pages.firstElementChild.cloneNode(true); // в качестве шаблона берём первый элемент
+  pages.firstElementChild.remove(); // и удаляем его из HTML
 
-    return (data, state, action) => {
-        // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
+  return (data, state, action) => {
+    // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
+    const rowsPerPage = state.rowsPerPage; // сколько строк выводим на одной странице
+    const pageCount = Math.ceil(data.length / rowsPerPage); // число страниц (округляем в большую сторону)
+    let page = state.page; // текущая страница (может меняться ниже)
 
-        // @todo: #2.6 — обработать действия
+    // @todo: #2.6 — обработать действия
+    if (action)
+      switch (action.name) {
+        case "prev":
+          page = Math.max(1, page - 1);
+          break; // на предыдущую
+        case "next":
+          page = Math.min(pageCount, page + 1);
+          break; // на следующую
+        case "first":
+          page = 1;
+          break; // на первую
+        case "last":
+          page = pageCount;
+          break; // на последнюю
+      }
 
-        // @todo: #2.4 — получить список видимых страниц и вывести их
+    // @todo: #2.4 — получить список видимых страниц и вывести их
+    const visiblePages = getPages(page, pageCount, 5); // получаем массив из максимум 5 страниц
+    pages.replaceChildren(
+      ...visiblePages.map((pageNumber) => {
+        const el = pageTemplate.cloneNode(true); // клонируем шаблон
+        return createPage(el, pageNumber, pageNumber === page); // заполняем его данными через коллбэк
+      }),
+    );
 
-        // @todo: #2.5 — обновить статус пагинации
+    // @todo: #2.5 — обновить статус пагинации
+    fromRow.textContent = (page - 1) * rowsPerPage + 1; // с какой строки выводим
+    toRow.textContent = Math.min(page * rowsPerPage, data.length); // до какой строки выводим
+    totalRows.textContent = data.length; // сколько всего строк
 
-        // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        return data.slice(0, 10);
-    }
-}
+    // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
+    const skip = (page - 1) * rowsPerPage; // сколько строк пропустить
+    return data.slice(skip, skip + rowsPerPage); // возвращаем только нужный кусок массива
+  };
+};
--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,18 +8,52 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  before.reverse().forEach((subName) => {
+    // Клонируем по имени и сохраняем прямо в объект root (чтобы потом иметь к ним доступ)
+    root[subName] = cloneTemplate(subName);
+    // Вставляем В НАЧАЛО контейнера таблицы
+    root.container.prepend(root[subName].container);
+  });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+  // Добавляем шаблоны ПОСЛЕ таблицы
+  after.forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    // Вставляем В КОНЕЦ контейнера таблицы
+    root.container.append(root[subName].container);
+  });
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+  root.container.addEventListener("change", () => {
+    onAction();
+  });
 
-    return {...root, render};
+  // Событие reset — когда форма сбрасывается (нужна задержка setTimeout)
+  root.container.addEventListener("reset", () => {
+    setTimeout(onAction);
+  });
+
+  // Событие submit — отправка формы (предотвращаем перезагрузку страницы!)
+  root.container.addEventListener("submit", (e) => {
+    e.preventDefault(); // Отключаем стандартное поведение браузера
+    onAction(e.submitter); // Передаем кнопку, которая вызвала отправку
+  });
+
+  const render = (data) => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+      object.keys(item).forEach((key) => {
+        if (row.element[key]) {
+          row.elements[key].textContent = item[key];
+        }
+      });
+      return (row, container);
+    });
+    root.elements.rows.replaceChildren(...nextRows);
+  };
+
+  return { ...root, render };
 }
